4cc27808d4e8677742a3991629475f4d69fce90d
Ben Hong <ben@bencodezen.io>
2023-03-23 18:00:09 -0700

    feat: add initial skeleton

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Simple Nuxt 3 Proxy App</h1>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ server/routes/fapi/[...].ts
@@ -0,0 +1,3 @@
+// The [...].ts is a catch all route
+// Official Docs: https://nuxt.com/docs/guide/directory-structure/server#catch-all-route
+export default eventHandler(() => ({}))
